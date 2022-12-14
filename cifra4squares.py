f146ebd1b802e6f2098d487c85023498709f2ae7
Fellipe <fellipe1311.fb@gmail.com>
2022-12-14 14:28:40 -0300

    Grille commit

--- cifra4squares.py
+++ cifra4squares.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import random
 
 def criptografar(mensagem):
@@ -96,3 +97,103 @@ print('done')
 
 
 
+=======
+import random
+
+def criptografar(mensagem):
+    key_1 = random.sample(letras, k =25)
+    quadrado_1 = [key_1[:5],key_1[5:10],key_1[10:15],key_1[15:20],key_1[20:25]]
+    key_2 = random.sample(letras, k =25)
+    quadrado_2 = [key_2[:5],key_2[5:10],key_2[10:15],key_2[15:20],key_2[20:25]]
+    coordenadas = []
+    for x in mensagem:
+        for y in letras_padrao:
+            for z in y:
+                if z == x:
+                    linha, coluna = letras_padrao.index(y),y.index(z)
+                    coordenadas.append([linha,coluna])
+    cifra_r = []
+    for i in range(len(coordenadas)):
+        if i %2 == 0:
+            cifra_r.append(quadrado_1[coordenadas[i][0]][coordenadas[i+1][1]])
+        elif i %2 != 0:
+            cifra_r.append(quadrado_2[coordenadas[i][0]][coordenadas[i-1][1]])
+    return cifra_r, key_1, key_2
+
+
+
+def decriptografar(mensagem_c,key_1,key_2):
+        quadrado_1 = [key_1[:5],key_1[5:10],key_1[10:15],key_1[15:20],key_1[20:25]]
+        quadrado_2 = [key_2[:5],key_2[5:10],key_2[10:15],key_2[15:20],key_2[20:25]]
+        cont = 0
+        cord = []
+        for x in mensagem_c:
+            if cont %2 ==0:
+                for y in quadrado_1:
+                    for z in y:
+                        if x == z:
+                            linha, coluna = quadrado_1.index(y), y.index(z)
+                            cord.append([linha,coluna])
+                            cont += 1
+            elif cont %2 != 0:
+                for y in quadrado_2:
+                    for z in y:
+                        if x == z:
+                            linha, coluna = quadrado_2.index(y), y.index(z)
+                            cord.append([linha,coluna])
+                            cont += 1
+        msg_d = []
+        for i in range(len(cord)):
+            if i %2 == 0:
+                msg_d.append(letras_padrao[cord[i][0]][cord[i+1][1]])
+            elif i %2 != 0:
+                msg_d.append(letras_padrao[cord[i][0]][cord[i-1][1]])
+        if msg_d[-1] == 'x':
+            msg_d = msg_d[:-1]
+        return msg_d
+
+
+
+letras = ['A','B','C','D','E','F','G','H','I','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+letras_padrao = [['a','b','c','d','e'],['f','g','h','i','k'],['l','m','n','o','p'],['q','r','s','t','u'],['v','w','x','y','z']]
+
+
+print('Oque você quer?\n1.Cifrar \n2.Decifrar')
+
+resp = input()
+sair = False
+
+
+while sair == False:
+    if resp == '1' or resp == 'Cifrar':
+        print('Digite sua mensagem')
+        mensagem = str(input())
+        mensagem.lower()
+        if len(mensagem) %2 != 0:
+            mensagem = mensagem + 'x'
+            mensagem_cr,key_1_cr, key_2_cr = criptografar(mensagem)
+            
+            print(f'mensagem: {"".join(mensagem_cr)}\nChave 1: {"".join(key_1_cr)}\nchave 2: {"".join(key_2_cr)}')
+            sair = True
+        else:
+            mensagem_cr,key_1_cr, key_2_cr = criptografar(mensagem)
+            
+            print(f'mensagem: {"".join(mensagem_cr)}\nChave 1: {"".join(key_1_cr)}\nchave 2: {"".join(key_2_cr)}')
+            sair = True
+  
+    elif resp == '2' or resp == 'Decifrar':
+        mensagem_c = input('digite a mensagem')
+        key_1 = input('digite a primeira chave')
+        key_2 = input('digite a segunda chave')
+        msg_d = decriptografar(mensagem_c,key_1,key_2)
+        print(''.join(msg_d))
+        sair = True   
+    else:
+        print('Digite 1 ou 2')
+        resp = input()
+
+print('done')
+
+
+
+>>>>>>> be0c9e3 (Grille commit)
--- grille.py
+++ grille.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import random
 import string
 mensagem = input('Digite sua mensagem ').replace(' ','')
@@ -26,3 +27,62 @@ for i in range(len(key)):
         decode.append(mensagem_cifrada[i])
 
 print(''.join(decode))
+=======
+import random
+import string
+
+
+def codificar(mensagem):
+    key = []
+    mensagem_cifrada = []
+    contador = len(mensagem)
+    index_da_letra = 0
+    for i in range(len(mensagem)*2):
+        if random.randint(0,1) == 1 and contador != 0 or (len(mensagem)*2) - i == contador:
+            mensagem_cifrada.append(mensagem[index_da_letra])
+            contador -=1
+            index_da_letra += 1
+            key.append('1')
+        elif i != len(mensagem)*2+1:
+            randomLetter = random.choice(string.ascii_letters)
+            mensagem_cifrada.append(randomLetter)
+            key.append('0')
+    return mensagem_cifrada, key
+
+
+def decodificar(mensagemcif,chave):
+    decode = []
+    for i in range(len(chave)):
+        if chave[i] == '1':
+            decode.append(mensagemcif[i])
+
+    return(decode)
+
+
+
+print('Oque você quer?\n1.Cifrar \n2.Decifrar')
+
+resp = input()
+sair = False
+
+
+while sair == False:
+    if resp == '1' or resp == 'Cifrar':
+        print('Digite sua mensagem')
+        mensagem = str(input().replace(' ',''))
+        msgc,chave = codificar(mensagem)
+        print(f'mensagem cifrada: {"".join(msgc).upper()}')
+        print(f'chave: {"".join(chave)}')
+        sair = True
+    elif resp == '2' or resp == 'Decifrar':
+        mensagem_c = input('digite a mensagem\n')
+        key = input('digite a chave\n')
+        msg_d = decodificar(mensagem_c,key)
+        print(''.join(msg_d))
+        sair = True   
+    else:
+        print('Digite 1 ou 2')
+        resp = input()
+
+print('done')
+>>>>>>> be0c9e3 (Grille commit)
